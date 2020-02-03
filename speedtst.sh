6f133768b6294364d33bf1a91a76c884b2443b3c
cryptoz <jayotis@outlook.com>
2020-02-02 19:54:08 -0700

    Update speedtst.sh

--- speedtst.sh
+++ speedtst.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 IFS=$'\n'
-arr=($(speedtest-cli --list))
+arr=($(speedtest-cli --list)) #get list of servers
 unset IFS
-read -r -d ')' id <<< "${ISP}"
+arsize=${#arr[@]}
 for i in "${arr[@]}"
 do
-	if echo $i | grep -q "Telus";  #test your ISP first
-	then
-		read -r -d ')' id <<< $i && break
-	fi
+        if echo $i | grep -q "Telus";    #test your ISP first
+        then
+                read -r -d ')' id <<< $i && break
+        fi
 done
 unset IFS
 date
 speedtest-cli --server $id
-arsize=${#arr[@]}
 top=(arsize / 10) #top 10%
 for ((i=0;i<=3;i++)); do
-	echo $'\n''Next server'
-	date
-	random=$[RANDOM%arsize]
-	read -r -d ')' id <<< "${arr[$random]}" #Randomly test a server (probably) outside your ISP network
-	speedtest-cli --server $id
+        echo $'\n''Next server'
+        date
+        random=$[RANDOM%top]
+        read -r -d ')' id <<< "${arr[$random]}"  #test 4 random servers (probably) outside your ISP network
+        speedtest-cli --server $id
 done
 exit
+
 
